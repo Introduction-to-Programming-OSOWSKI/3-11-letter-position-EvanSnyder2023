846fda15dbfb24be39b6336b60b4d6b191d7d056
EvanSnyder2023 <816547@mystma.org>
2021-02-05 00:04:38 +0000

    LetterPos try 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(w, x):
+
+    count = 0
+    for i in range(0,len(w)+ 1):
+        if w[i- 1] == x:
+            count = i
+            return count
+    else:
+        return "none"
+    
+
+print(letterPos("sunshine", "n"))
